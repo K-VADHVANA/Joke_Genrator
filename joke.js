aff4e104551617756965a7efb5ba97b5b583b866
khushi vadhvana <98330791+K-VADHVANA@users.noreply.github.com>
2024-04-12 12:23:09 +0530

    Add files via upload
    A practice application of JS concept

---
+++ joke.js
@@ -0,0 +1,329 @@
+joke = [
+    {
+    "category": "Programming",
+    "type": "single",
+    "joke": "If Bill Gates had a dime for every time Windows crashed ... Oh wait, he does.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 22,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Debugging: Removing the needles from the haystack.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 40,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"Can I tell you a TCP joke?\"\n\"Please tell me a TCP joke.\"\n\"OK, I'll tell you a TCP joke.\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 57,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "I'll never forget my Granddad's last words to me just before he died. \"Are you still holding the ladder?\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 208,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "The glass is neither half-full nor half-empty, the glass is twice as big as it needs to be.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 23,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "Dark humor is like food, not everyone gets it.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": true,
+        "sexist": false,
+        "explicit": true
+    },
+    "id": 162,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "I didn't vaccinate my 10 kids and the one that survived is fine!",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 96,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "Me and my Jewish friend were eating lunch and I farted. He got mad so I said \"cmon man a little gas never killed anyone\".",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": true,
+        "sexist": false,
+        "explicit": true
+    },
+    "id": 103,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Java is like Alzheimer's, it starts off slow, but eventually, your memory is gone.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 27,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "There are only 10 kinds of people in this world: those who know binary and those who don't.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 35,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Your momma is so fat, you need to switch to NTFS to store a picture of her.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+    },
+    "id": 55,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "twopart",
+    "setup": "What do you call a developer who doesn't comment code?",
+    "delivery": "A developer.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 14,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "A byte walks into a bar looking miserable.\nThe bartender asks it: \"What's wrong buddy?\"\n\"Parity error.\" it replies. \n\"Ah that makes sense, I thought you looked a bit off.\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 24,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Programming is 10% science, 20% ingenuity, and 70% getting the ingenuity to work with the science.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 37,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Debugging: Removing the needles from the haystack.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 40,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "The six stages of debugging:\n1. That can't happen.\n2. That doesn't happen on my machine.\n3. That shouldn't happen.\n4. Why does that happen?\n5. Oh, I see.\n6. How did that ever work?",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 123,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "twopart",
+    "setup": "What do you get if you lock a monkey in a room with a typewriter for 8 hours?",
+    "delivery": "A regular expression.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 26,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "twopart",
+    "setup": "Why did the Python programmer not respond to the foreign mails he got?",
+    "delivery": "Because his interpreter was busy collecting garbage.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 15,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "twopart",
+    "setup": "Hey, wanna hear a joke?",
+    "delivery": "Parsing HTML with regex.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 10,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "twopart",
+    "setup": "Why does no one like SQLrillex?",
+    "delivery": "He keeps dropping the database.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 13,
+    "safe": true,
+    "lang": "en"
+}
+]
+let index = Math.floor( Math.random( )* (joke.length-1))
+jokes.innerHTML = joke[index].joke
